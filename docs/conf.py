d0d42fc8c668742d890498c5398b415d74592e18
EDI-N <45854577+EDI-N@users.noreply.github.com>
2018-12-29 15:31:48 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -148,7 +148,7 @@ html_logo = "demo/static/logo_edin2.gif"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['demo/static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
