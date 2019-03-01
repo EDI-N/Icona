f96c46e0b9e87f7fd109ea6e91da510daf19e19f
Kolya Bezgin <bezginn@edi-n.com>
2019-03-01 16:31:00 +0200

    123123

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ language = ['ru', 'uk_UA', 'en']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = []
+exclude_patterns = ['_build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
